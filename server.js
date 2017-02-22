c5bc729dd3f1c5bdc0fef3d4459a7c02607dbe57
krissnickov <kriss@emnovate.com>
2017-02-22 09:04:18 -0500

    KN#002 changing the port where the app is listening

--- server.js
+++ server.js
@@ -55,4 +55,14 @@ app.get("/", function (req, res) {
 });
 
 var server = http.createServer(app);
-server.listen(3000);
+server.listen(process.env.PORT || 8080);
+
+//var http = require('http');
+
+
+
+//http.createServer(function (req, res) {
+//    res.writeHead(200, { 'Content-Type': 'text/html' });
+//    res.end('Hello, world!');
+//
+//}).listen(process.env.PORT || 8080);
